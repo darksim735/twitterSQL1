21a2a8973f1d154edac66f905420894e98700219
darksim735 <darksim735@gmail.com>
2015-02-21 18:07:48 -0500

    same for user.js

---
+++ models/user.js
@@ -0,0 +1,13 @@
+var Sequelize = require('sequelize');
+
+module.exports = function(sequelize) {
+    var User = sequelize.define('User', {
+        name: Sequelize.STRING,
+        pictureUrl: Sequelize.STRING
+    }, {
+        timestamps: false // this will deactivate the time columns
+    });
+
+    return User;
+
+};
